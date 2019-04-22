36fa0a9d66dfeba7e7cea4b9cecb404fcc95e349
alex maslakoff <alex@serendipia.email>
2019-04-22 15:05:40 -0500

    wip

--- zomes/orion_project1/code/src/offer/mod.rs
+++ zomes/orion_project1/code/src/offer/mod.rs
@@ -1,15 +1,33 @@
 use hdk::{
     self,
     entry_definition::ValidatingEntryType,
+    error::ZomeApiResult
 };
 
+use hdk::holochain_core_types::{
+    cas::content::Address,
+    entry::Entry,
+    error::HolochainError,
+    json::{JsonString,RawString},
+    hash::HashString,
+};
 
 struct Offer {
-    id: i64,
+    id: HashString,
     exchange_id: i64,
     broker_id: i64,
-    currency_code: String,
-    quantity: i64,
-    price: i64,
+    base_asset: String,
+    quoted_asset: String,
+    side: Side,
+
+    //todo: these may be replaced with 'decimal' if needed
+    price: f64,
+    quantity: f64,
+
     inserted_at: i64
+}
+
+enum Side {
+  Buy,
+  Sell
 }
