cf121140c79477960407f36522428f8490a1d584
alex maslakoff <alex@serendipia.email>
2019-05-01 17:35:01 -0500

    wip

--- test/index.js
+++ test/index.js
@@ -7,10 +7,9 @@ const dna = Config.dna(dnaPath)
 const instanceAlice = Config.instance(agentAlice, dna)
 const scenario = new Scenario([instanceAlice])
 
+
+// todo
 scenario.runTape("create broker", async (t, { alice }) => {
     const addr = alice.call("broker", "create_broker", {"name": "broker1"})
-    //  const result = alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
-
-    // check for equality of the actual and expected results
-    t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"name": "broker1"}' ] } })
+    t.deepEqual(result, { Ok: { App: ["broker", '{"name": "broker1"}' ] } })
 })
