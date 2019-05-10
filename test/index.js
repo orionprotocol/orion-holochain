d34c0707c8424f390cb13b2f7791a94623eadda0
alex maslakoff <alex@serendipia.email>
2019-05-10 17:44:06 -0400

    wip

--- test/index.js
+++ test/index.js
@@ -14,9 +14,14 @@ scenario.runTape("create broker", async (t, { alice }) => {
 
     // todo:
     // t.deepEqual(res1, { Ok: 'QmYfmS8M4EZRkpkNqyAtCgPaMjguAnbwupvRfpASYo4j9k' });
+})
 
+scenario.runTape("create order", async (t, { alice }) => {
+    //todo: why will it pass?
+    const res = alice.call("orion_project1", "initialize_order", {"name": "name1", /*todo*/});
+    var {Ok: addr} = res;
+    t.deepEqual(res.Ok, addr);
 
-    const res2 = alice.call("orion_project1", "initialize_order", {"??name": "todo??"});
-    var {Ok: addr2} = res2;
-    t.deepEqual(res2.Ok, addr2);
+    const res2 = alice.call("orion_project1", "approve_order", {"addr": addr});
+    t.equal(res.Err, undefined);
 })
